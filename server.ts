496aa2b1c1bc2f6afa883f575836babfdfa82b18
davidluzhiyun <david.luzhiyun@gmail.com>
2024-09-11 01:27:08 -0400

    Initial set up for server

---
+++ server.ts
@@ -0,0 +1,14 @@
+import express from "express";
+// create application
+const app = express();
+// set port
+const port = 3000;
+
+// set the ./public directory as the static directory
+app.use(express.static("public"));
+
+// starts the server listening on the specified port
+app.listen(port, ()=>{
+    console.log("Homework 2 listening on port ${port}");
+});
+
