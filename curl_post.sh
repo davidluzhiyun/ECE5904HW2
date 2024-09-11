9d552097b0ba4d6e8ab7e3e6fae9aaa62cc5325d
davidluzhiyun <david.luzhiyun@gmail.com>
2024-09-11 05:22:03 -0400

    Finally figured out how to make these work, use git bash

--- curl_post.sh
+++ curl_post.sh
@@ -1,5 +1,5 @@
 echo "Adding a location to favorite"
-curl -d '{"name":"Durham, North Carolina", "zip":"27708"}' \
+curl -d '{"name":"North Carolina, Durham", "zip":"27708"}' \
      -H "Content-Type: application/json" \
      -X POST \
      http://localhost:3000/favorites
