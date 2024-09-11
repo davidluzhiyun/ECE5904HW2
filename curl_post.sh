40f68a0edfbb2efdae0f563ac055530934d0729a
davidluzhiyun <david.luzhiyun@gmail.com>
2024-09-11 05:40:18 -0400

    Tested the API with shell in git bash, added comment about not working in powershell

--- curl_post.sh
+++ curl_post.sh
@@ -1,3 +1,5 @@
+# doesn't work with powershell even with curl installed and curl alias for Invoke-WebRequest removed
+# works with git bash
 echo "Adding a location to favorite"
 curl -d '{"name":"North Carolina, Durham", "zip":"27708"}' \
      -H "Content-Type: application/json" \
